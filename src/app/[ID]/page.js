cead214428f31001e6734be4f6d032e15487208f
jmorofsky <jmorofsky@gmail.com>
2025-10-20 23:29:30 -0400

    add invalid post handling

--- src/app/[ID]/page.js
+++ src/app/[ID]/page.js
@@ -5,8 +5,17 @@ export default async function Post({ params }) {
 
     const db = new Database("./src/_db/posts.db", { fileMustExist: true })
     db.pragma('journal_mode = WAL')
-    
+
     const selectedPost = db.prepare(`SELECT * FROM posts WHERE ID = ${ID}`).get()
+    if (selectedPost == undefined) {
+        return (
+            <div className='text-center text-primary-text mt-[30vh] m-auto'>
+                <h1 className='text-6xl font-black'>404</h1>
+                <p className='mt-3 text-secondary-text text-lg'>That post doesn't exist.</p>
+            </div>
+        )
+    }
+
     const title = selectedPost.Title
     const date = selectedPost.date
     const edited = selectedPost.Edited
