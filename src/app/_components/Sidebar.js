c17914c488e44ccaa2cb54b7ca8f3ad383fafab2
jmorofsky <jmorofsky@gmail.com>
2025-10-20 21:40:25 -0400

    add hover effect to links

--- src/app/_components/Sidebar.js
+++ src/app/_components/Sidebar.js
@@ -25,19 +25,25 @@ export default function Sidebar() {
                 <div className='self-start mt-8 ml-2 text-primary-text font-mono italic'>
                     <Link href="https://www.linkedin.com/in/jason-morofsky/">
                         <Image src={linkedin} alt='LinkedIn' className='h-6 w-9 inline' />
-                        <span className='underline'>LinkedIn</span>&nbsp;
+                        <span className='underline hover:text-secondary-text transition duration-150'>
+                            LinkedIn
+                        </span>&nbsp;
                         <span className='text-sm'>↗</span>
                     </Link>
                     <br />
                     <Link href="https://github.com/jmorofsky/" className='pl-2'>
                         <Image src={github} alt='GitHub' className='h-4 w-4 inline' />
-                        <span className='underline pl-3'>GitHub</span>&nbsp;
+                        <span className='underline pl-3 hover:text-secondary-text transition duration-150'>
+                            GitHub
+                        </span>&nbsp;
                         <span className='text-sm'>↗</span>
                     </Link>
                     <br />
                     <Link href="mailto:contact@jasonmorofsky.com" className='pl-[7px]'>
                         <span className='not-italic text-lg text-[#FFF]'>✉</span>
-                        <span className='underline pl-3'>Email</span>&nbsp;
+                        <span className='underline pl-3 hover:text-secondary-text transition duration-150'>
+                            Email
+                        </span>&nbsp;
                         <span className='text-sm'>↗</span>
                     </Link>
                 </div>
