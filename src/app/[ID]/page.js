c40f9608e8256be94dc2905e45b9d50365149a35
jmorofsky <jmorofsky@gmail.com>
2025-10-23 22:30:51 -0400

    add post page

--- src/app/[ID]/page.js
+++ src/app/[ID]/page.js
@@ -1,4 +1,7 @@
 import Database from 'better-sqlite3'
+import { promises as fs } from 'fs'
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 export default async function Post({ params }) {
     const { ID } = await params
@@ -17,10 +20,36 @@ export default async function Post({ params }) {
     }
 
     const title = selectedPost.Title
-    const date = selectedPost.date
+    const date = selectedPost.Date
     const edited = selectedPost.Edited
 
+    let postContent = ''
+    try {
+        const path = `./src/_assets/post_md/${ID}.md`
+        postContent = await fs.readFile(path, 'utf8')
+    } catch (error) {
+        return (
+            <div className='text-center text-primary-text mt-[30vh] m-auto'>
+                <h1 className='text-6xl font-black'>500</h1>
+                <p className='mt-3 text-secondary-text text-lg'>There was a problem loading this post.</p>
+            </div>
+        )
+    }
+
     return (
-        <div className="text-primary-text ml-[500px] mt-20">{title}</div>
+        <>
+            <div className="flex flex-col mt-24 mb-15 px-15 gap-3">
+                <p className='text-secondary-text font-mono text-sm'>
+                    🗓 {date}{edited && <em className='text-accent'>&nbsp;&nbsp;[Edited: {edited}]</em>}
+                </p>
+                <hr className='text-gold-accent' />
+
+                <h1 className='text-primary-text text-5xl mb-5'>{title}</h1>
+
+                <div className='prose'>
+                    <Markdown remarkPlugins={[remarkGfm]}>{postContent}</Markdown>
+                </div>
+            </div>
+        </>
     )
 }
