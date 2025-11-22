d68789aae58d13b5c6e3e7a3f76bdb1f53c15898
jmorofsky <jmorofsky@gmail.com>
2025-11-22 18:57:56 -0500

    fix exception when navigating to invalid post

--- src/app/[ID]/_components/Post.js
+++ src/app/[ID]/_components/Post.js
@@ -11,22 +11,14 @@ import NotFoundPage from '@/app/_shared/NotFoundPage';
 
 export default function Post(props) {
     const id = props.id;
-    const image = require(`@/_assets/post_images/${id}.png`);
 
     const [error, setError] = useState('');
     const [notFound, setNotFound] = useState(false);
     const [title, setTitle] = useState('');
+    const [image, setImage] = useState(null);
     const [content, setContent] = useState('');
     const [date, setDate] = useState('');
     const [edited, setEdited] = useState('');
-
-    if (error) {
-        return <ErrorPage error={error} />;
-    };
-
-    if (notFound) {
-        return <NotFoundPage />;
-    };
 
     useEffect(() => {
         getPostData(id)
@@ -37,6 +29,7 @@ export default function Post(props) {
                     setNotFound(true);
                 } else {
                     setTitle(response.title);
+                    setImage(require(`@/_assets/post_images/${id}.png`));
                     setContent(response.content);
                     setDate(response.date);
                     setEdited(response.edited);
@@ -46,6 +39,14 @@ export default function Post(props) {
                 setError('getPostData');
             });
     }, []);
+
+    if (error) {
+        return <ErrorPage error={error} />;
+    };
+
+    if (notFound) {
+        return <NotFoundPage />;
+    };
 
     return (
         <div className='max-w-200'>
@@ -58,11 +59,15 @@ export default function Post(props) {
             <hr className='mt-4 mb-1 text-gold-accent' />
             <h1 className='text-primary-text text-3xl mb-5 min-[600px]:text-5xl'>{title}</h1>
 
-            <Image
-                src={image}
-                alt=''
-                className='mb-8 rounded-sm object-cover ring-4 ring-[#202020]'
-            />
+            {image ?
+                <Image
+                    src={image}
+                    alt=''
+                    className='mb-8 rounded-sm object-cover ring-4 ring-[#202020]'
+                />
+                :
+                null
+            }
 
             <div className='prose max-w-none'>
                 <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
